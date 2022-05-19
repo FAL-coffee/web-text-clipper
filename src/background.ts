3fd7a6f1c34657953099f814074c6d23ecf3b41f
FAL-coffee <fal.engineer.2001@gmail.com>
2022-05-19 22:14:41 +0900

    add semicolon

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,6 @@
 // browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 //   console.log('Hello from the background')
 // })
-browser.runtime.onMessage.addListener(function () {
-  console.log('Hello from the background')
-})
+chrome.runtime.onMessage.addListener(function () {
+  console.log('Hello from the background');
+});
